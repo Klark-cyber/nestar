9e30bec226bb12e63eb05b6ba8a90df9dde48d14
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-03-25 23:37:13 +0900

    feat: configure env variables

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config"; //.env ichidagilarni import qilish imkonini beruvchi package
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
+import { ConfigModule } from "@nestjs/config"; //.env ichidagilarni import qilish imkonini beruvchi package
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestarBatchController],
   providers: [NestarBatchService],
 })
 export class NestarBatchModule {}
-console.log("keldi")
 
 
