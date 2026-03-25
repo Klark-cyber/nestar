38bb7e01173e7deea37a00022f302dfd041ce157
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-03-25 23:27:50 +0900

    feat: develop nestar monorepo project

---
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { NestarBatchController } from './nestar-batch.controller';
+import { NestarBatchService } from './nestar-batch.service';
+
+@Module({
+  imports: [],
+  controllers: [NestarBatchController],
+  providers: [NestarBatchService],
+})
+export class NestarBatchModule {}
+console.log("keldi")
+
+
