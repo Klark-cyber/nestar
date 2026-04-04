2803534cdbf3b41366c288fbe7741c980ae929db
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-04 18:14:58 +0900

    fix: develop login business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -21,13 +21,13 @@ export class MemberResolver {
         }
     }
 
-    @Mutation(() => String) 
+    @Mutation(() => Member) 
     @UsePipes(ValidationPipe) //ushbu integratsiya orqali pipe validationning method darajasidagi qonuniyatini integratsiya qilamiz.Agar bu qatorni klassdan tashqarisiga yozsak resolver darajadagi integratsiya bolar edi
-    public async login(@Args("input") input: LoginInput ): Promise<string> {
+    public async login(@Args("input") input: LoginInput ): Promise<Member> {
          try{
         console.log("Mutation: signup");
         console.log("input:", input);
-        return this.memberService.login();
+        return this.memberService.login(input);
         }catch(err){
         console.log("Error, signup:", err)
         throw new InternalServerErrorException(err);
