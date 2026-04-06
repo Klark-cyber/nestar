aa31825ea9b16f71357dc49a051e663eca630136
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-06 12:03:55 +0900

    feat: standart integration of logging interceptor

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()) //=> pipelarni global tarzda ornatamiz.
+  app.useGlobalInterceptors(new LoggingInterceptor()) //=> Logging interceptorni hosil qildik natijada terminalda re/res haqida bazi malumotlarga ega bolamiz
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
