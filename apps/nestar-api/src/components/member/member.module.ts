ed04181144c4f5cae117a1f62577db400b10fe7e
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-12 20:31:15 +0900

    feat: modify viewModel to record Views

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -4,11 +4,13 @@ import { MemberService } from './member.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: "Member", schema: MemberSchema }]),
-    forwardRef(() => AuthModule) // <--- AuthModule shu yerda bo'lishi shart
+    forwardRef(() => AuthModule), // <--- AuthModule shu yerda bo'lishi shart
+    ViewModule,
   ],
   providers: [MemberResolver, MemberService],
   exports: [MemberService], // Buni ham qo'shib qo'yish zarar qilmaydi
