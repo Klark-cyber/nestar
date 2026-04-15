572abeba5ce53270bf90e9eaaa68f953d44556a9
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-15 20:18:19 +0900

    fix: modify upload images configuration

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,14 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import {graphqlUploadExpress} from "graphql-upload" //serverga klayotgan file hajmiga limit qoyish uchun kerak
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("STEP-1")
   app.useGlobalPipes(new ValidationPipe()) //=> pipelarni global tarzda ornatamiz.
-  console.log("STEP-2")
   app.useGlobalInterceptors(new LoggingInterceptor()) //=> Logging interceptorni hosil qildik natijada terminalda re/res haqida bazi malumotlarga ega bolamiz
-  console.log("STEP-8")
+  app.enableCors({origin:true, credentials:true}) //Cors integratsiyasini amalga oshirdik frontenddan yuborilgan file yoki rasmni qabul qilish uchun
+  app.use(graphqlUploadExpress({maxFileSize: 150000000, maxFiles: 10}));
+  app.use("/uploads", express.static('./uploads')) //uploads folderni static qildik
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
