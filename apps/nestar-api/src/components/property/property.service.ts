daf2a90a5d4f77025b1f0de34d771da82ca5fef0
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-18 11:50:27 +0900

    feat: develop updateProperty business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -10,6 +10,8 @@ import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { Member } from '../../libs/dto/member/member';
 import { ViewService } from '../view/view.service';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+
 
 @Injectable()
 export class PropertyService {
@@ -69,4 +71,37 @@ export class PropertyService {
    )
    .exec();
  }
+
+ public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+  let { propertyStatus, soldAt, deletedAt } = input;
+  const search: T = {
+   _id: input._id,
+   memberId: memberId,
+   propertyStatus: PropertyStatus.ACTIVE,
+  };
+
+  if (propertyStatus === PropertyStatus.SOLD) soldAt = new Date();
+  else if (propertyStatus === PropertyStatus.DELETE) deletedAt = new Date();
+
+  const result = await this.propertyModel
+   .findOneAndUpdate(search, input, {
+    new: true,
+   })
+   .exec();
+  if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+  if (soldAt || deletedAt) {
+   await this.memberService.memberStatsEditor({
+    _id: memberId,
+    targetKey: 'memberProperties',
+    modifier: -1,
+   });
+  }
+
+  return result;
+ }
+}
+
+function moment() {
+  throw new Error('Function not implemented.');
 }
