f505389c363b2e6a241f4a25fb168cf8f8a8f99f
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-20 20:10:58 +0900

    feat: create comment update comment getComments GraphQl Apis

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -147,6 +147,7 @@ export class MemberService {
 
     public async memberStatsEditor(input: StatisticModifier): Promise<Member>{  //memberga dahldor kerakli qiymatni ozgartirish imkonini beruvchi method
         const {_id, targetKey, modifier } = input;
-        return await this.memberModel.findOneAndUpdate(_id, {$inc: {[targetKey]: modifier}}, {new: true}).exec() as Member; //masalan {memberProperties: 1}
+        return await this.memberModel.findByIdAndUpdate(_id, {$inc: {[targetKey]: modifier}}, {new: true}).exec() as Member; //masalan {memberProperties: 1}
     }
+    
 }
