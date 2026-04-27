a076b8bfc6dc9d7558994686778e09f5da28adc5
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-04-27 20:03:14 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -13,8 +13,9 @@ export class BatchController {
   handleTimeout() {
     this.logger.debug('BATCH SERVER READY');
   }
+//                  JobSchedulers
 
-@Cron('00 * * * * *', { name: 'BATCH_ROLLBACK' })
+@Cron('00 00 01 * * *', { name: 'BATCH_ROLLBACK' })
   public async batchRollback() {
     try {
       this.logger['context'] = BATCH_ROLLBACK;
@@ -25,19 +26,19 @@ export class BatchController {
     }
   }
 
-  @Cron('20 * * * * *', { name: 'BATCH_TOP_PROPERTIES' })
-  public async batchProperties() {
+  @Cron('20 00 01 * * *', { name: 'BATCH_TOP_PROPERTIES' })
+  public async batchTopProperties() {
     try {
       this.logger['context'] = BATCH_TOP_PROPERTIES;
       this.logger.debug('EXECUTED!');
-      await this.batchService.batchProperties();
+      await this.batchService.batchTopProperties();
     } catch (err) {
       this.logger.error(err);
     }
   }
 
-  @Cron('40 * * * * *', { name: 'BATCH_TOP_AGENTS' })
-  public async batchAgents() {
+  @Cron('40 00 01 * * *', { name: 'BATCH_TOP_AGENTS' })
+  public async batchTopAgents() {
     try {
      
     } catch (err) {
@@ -45,7 +46,7 @@ export class BatchController {
     }
     this.logger['context'] = BATCH_TOP_AGENTS;
     this.logger.debug('EXECUTED!');
-     await this.batchService.batchAgents();
+     await this.batchService.batchTopAgents();
   }
 
   /**
